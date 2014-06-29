0fdee5517627eb2cb6fea8d02a369944cd648b24
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-29 13:53:38 +1000

    Removed output options from the example

--- examples/simple.js
+++ examples/simple.js
@@ -2,9 +2,3 @@ var normalice = require('..');
 
 console.log(normalice('stun.l.google.com:19302'));
 // --> { urls: [ 'stun: stun.l.google.com:19302' ], url: 'stun:stun.l.google.com:19302' }
-
-console.log(normalice('stun.l.google.com:19302', { output: ['urls'] }));
-// --> { urls: [ 'stun: stun.l.google.com:19302' ] }
-
-console.log(normalice('stun.l.google.com:19302', { output: ['url'] }));
-// --> { url: 'stun: stun.l.google.com:19302' }
