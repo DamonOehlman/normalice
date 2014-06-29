646843258479576e1031635f7581021a43555094
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-29 13:11:18 +1000

    gendocs

--- examples/simple.js
+++ examples/simple.js
@@ -3,8 +3,8 @@ var normalice = require('..');
 console.log(normalice('stun.l.google.com:19302'));
 // --> { urls: [ 'stun: stun.l.google.com:19302' ], url: 'stun:stun.l.google.com:19302' }
 
-console.log(normalice('stun.l.google.com:19302'), { output: ['urls'] });
+console.log(normalice('stun.l.google.com:19302', { output: ['urls'] }));
 // --> { urls: [ 'stun: stun.l.google.com:19302' ] }
 
-console.log(normalice('stun.l.google.com:19302'), { output: ['url'] });
+console.log(normalice('stun.l.google.com:19302', { output: ['url'] }));
 // --> { url: 'stun: stun.l.google.com:19302' }
