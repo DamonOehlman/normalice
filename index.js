bc01cf3d7d2b3661adf1503be060f6e4db45c25b
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-29 13:09:46 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+  # normalice
+
+  Normalize an ice server configuration object (or plain old string) into a format
+  that is usable in all browsers supporting WebRTC.  Primarily this module is designed
+  to help with the transition of the `url` attribute of the configuration object to
+  the `urls` attribute.
+
+  ## Example Usage
+
+  <<< examples/simple.js
+
+**/
+
+module.exports = function(input, opts) {
+  // handle string input
+  if (typeof input == 'string' || (input instanceof of String)) {
+    input = {
+      url: input;
+    };
+  }
+};
