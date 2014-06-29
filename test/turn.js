261325346746ce18550f353fcf95d523a25d4f2e
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-29 13:53:04 +1000

    Finished tests

--- test/turn.js
+++ test/turn.js
@@ -1,49 +1,52 @@
 var test = require('tape');
 var normalice = require('..');
-var servers = require('./data/servers-url');
 
-test('normalizing turn url with embedded username:credential', function(t) {
-  var server;
+runTests(require('./data/servers-url'));
+runTests(require('./data/servers-string'));
 
-  t.plan(5);
-  t.ok(server = normalice(servers.turn[0]));
-  t.equal(server.url, 'turn:example.org:3478');
-  t.deepEqual(server.urls, ['turn:example.org:3478']);
-  t.equal(server.username, 'tmp');
-  t.equal(server.credential, 'test');
-});
+function runTests(servers) {
+  test('normalizing turn url with embedded username:credential', function(t) {
+    var server;
 
-return;
+    t.plan(5);
+    t.ok(server = normalice(servers.turn[0]));
+    t.equal(server.url, 'turn:example.org:3478');
+    t.deepEqual(server.urls, ['turn:example.org:3478']);
+    t.equal(server.username, 'tmp');
+    t.equal(server.credential, 'test');
+  });
 
-test('normalizing turn url with embedded username', function(t) {
-  var server;
+  test('normalizing turn url with embedded username', function(t) {
+    var server;
 
-  t.plan(5);
-  t.ok(server = normalice(servers.turn[1]));
-  t.equal(server.url, 'turn:example.org:3478');
-  t.deepEqual(server.urls, ['turn:example.org:3478']);
-  t.equal(server.username, 'tmp');
-  t.equal(server.credential, 'test');
-});
+    t.plan(5);
+    t.ok(server = normalice(servers.turn[1]));
+    t.equal(server.url, 'turn:example.org:3478');
+    t.deepEqual(server.urls, ['turn:example.org:3478']);
+    t.equal(server.username, 'tmp');
+    t.equal(server.credential, 'test');
+  });
 
-test('normalizing turn url with embedded username (no port)', function(t) {
-  var server;
+  test('normalizing turn url with embedded username (no port)', function(t) {
+    var server;
 
-  t.plan(5);
-  t.ok(server = normalice(servers.turn[2]));
-  t.equal(server.url, 'turn:example.org');
-  t.deepEqual(server.urls, ['turn:example.org']);
-  t.equal(server.username, 'tmp');
-  t.equal(server.credential, 'test');
-});
+    t.plan(5);
+    t.ok(server = normalice(servers.turn[2]));
+    t.equal(server.url, 'turn:example.org');
+    t.deepEqual(server.urls, ['turn:example.org']);
+    t.equal(server.username, 'tmp');
+    t.equal(server.credential, 'test');
+  });
 
-test('normalizing turn url with embedded username (transport specified)', function(t) {
-  var server;
+  test('normalizing turn url with embedded username (transport specified)', function(t) {
+    var server;
 
-  t.plan(5);
-  t.ok(server = normalice(servers.turn[3]));
-  t.equal(server.url, 'turn:example.org?transport=tcp');
-  t.deepEqual(server.urls, ['turn:example.org?transport=tcp']);
-  t.equal(server.username, 'tmp');
-  t.equal(server.credential, 'test');
-});
+    t.plan(5);
+    t.ok(server = normalice(servers.turn[3]));
+    t.equal(server.url, 'turn:example.org?transport=tcp');
+    t.deepEqual(server.urls, ['turn:example.org?transport=tcp']);
+    t.equal(server.username, 'tmp');
+    t.equal(server.credential, 'test');
+  });
+}
+
